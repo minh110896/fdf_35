77cde924687b81c69c6b0979831e33d2c6511e4f
minh110896 <hoangminh11081996@gmail.com>
2017-10-31 09:50:52 +0700

    Delete

--- spec/factories/product.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :product do
-    name {Faker::Name.name}
-    price 12000
-    quanlity 100
-    information {Faker::Lorem.sentence}
-    association :category, :factory => :category
-  end
-end
